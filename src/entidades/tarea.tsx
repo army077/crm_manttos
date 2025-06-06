a1e501e3559bd8ba29051a06114a95ef972b4868
Matamorrozz <matamorrozz@hotmail.com>
2025-06-06 16:34:59 -0600

    jiji

--- src/entidades/tarea.tsx
+++ src/entidades/tarea.tsx
@@ -1,14 +1,22 @@
-// src/types.ts
+// entidades/tarea.ts
 export type Etapa = "INBOX" | "NEGOCIACION" | "APARTADO" | "PAGADO" | "FINALIZADO";
+export type Prioridad = "alta" | "media" | "baja";
+
+export interface ChecklistItem {
+  id: number;
+  texto: string;
+  completado: boolean;
+}
 
 export interface Tarea {
   id: number;
   titulo: string;
   descripcion: string;
   etapa: Etapa;
-  etiquetas?: string[]; // Permite undefined
   fecha_vencimiento?: string;
   fecha_creacion?: string;
-  prioridad?: "alta" | "media" | "baja";
-  checklist?: { id: number; texto: string; completado: boolean }[];
+  etiquetas: string[];
+  prioridad: Prioridad;
+  checklist: ChecklistItem[];
+  deleted?: boolean;
 }
